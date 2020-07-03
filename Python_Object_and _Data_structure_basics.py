c04298b2d0724308fecb29dcbf0a440f22cb8793
Sagar Sharma <ss0315345@outlook.com>
2020-07-04 02:51:29 +0530

    Intro to numbers and expressions

--- Python_Object_and _Data_structure_basics.py
+++ Python_Object_and _Data_structure_basics.py
@@ -8,3 +8,22 @@
 # tuple -- Ordered immutable sequence of Objects, ex: (10, "hello", 200.3)
 # set -- Unordered collection of unique objects, ex: {"a","b"}
 # bool -- Logical value indicating True or False
+
+# Numbers
+# =======
+2 + 3
+3 - 2
+4 / 2
+3 * 2
+
+5 % 2  # Mod operator to get remainder
+2 ** 3  # power (2 to the power of 3)
+
+# Arithmetic expressions
+# ======================
+2+10*10+3 # This will result into 105
+(2+10) * (10+3) # This will result into 156
+
+# Variable assignments
+# ====================
+
